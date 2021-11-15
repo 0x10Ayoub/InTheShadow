6fa2d80d3ce0d300edf76a62658bc90fe37298a2
Ayoub Khourbach <akhourba@e1r4p1.1337.ma>
2021-11-15 22:57:41 +0100

    add lerpt rotation with moving lerp

--- Assets/Scripts/GamePlayControl.cs
+++ Assets/Scripts/GamePlayControl.cs
@@ -9,7 +9,6 @@ public class GamePlayControl : MonoBehaviour
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float movingSpeed;
     [SerializeField] private ShadowObject[] shadowObjects;
-
     private bool _gameEnded;
     private int _objectIndex;
     private Vector3 _dir;
@@ -18,26 +17,41 @@ public class GamePlayControl : MonoBehaviour
     {
         _gameEnded = false;
         _dir = Vector3.zero;
-        rotationSpeed *= Time.deltaTime;
-        movingSpeed *= Time.deltaTime;
         _objectIndex = 0;
     }
 
     private void Update()
     {
         SetInput();
-        Move(shadowObjects[_objectIndex]);
+        Control();
     }
 
+    private void Rotate(ShadowObject target)
+    {
+        if(!Input.GetMouseButton(0) || _gameEnded) return;
+        target.Rotate(_dir * 10f,rotationSpeed);
+    }
+
+    private void Control()
+    {
+        if(!Input.GetMouseButton(0) || _gameEnded) return;
+        ShadowObject target = shadowObjects[_objectIndex];
+        if(Input.GetKey(KeyCode.LeftControl))
+            Move(target);
+        else
+            Rotate(target);
+        if (target.CheckIfRotationCorrect() && target.CheckIfPositionCorrect())
+        {
+            _gameEnded = true;
+            //StartCoroutine(target.LerpToCorrectRotation());
+            StartCoroutine(target.LerpToCorrectPosition());
+        }
+        
+    }
     private void Move(ShadowObject target)
     {
         if(!Input.GetMouseButton(0) || _gameEnded) return;
-        target.Rotate(_dir,rotationSpeed);
-        if (target.CheckIfRotationCorrect())
-        {
-            _gameEnded = true;
-            StartCoroutine(target.LerpToRotation());
-        }
+        target.Move(_dir,movingSpeed);
     }
 
     private void SetInput()
--- Assets/Scripts/ShadowObject.cs
+++ Assets/Scripts/ShadowObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class ShadowObject : MonoBehaviour
@@ -9,32 +10,47 @@ public class ShadowObject : MonoBehaviour
     [SerializeField] private Transform shadowOnly;
     [SerializeField] private Transform meshOnly;
     [SerializeField] private Transform correctFormat;
+    [SerializeField] private Transform lightPoint;
     [SerializeField] private float rotationTolerance;
     [SerializeField] private float positionTolerance;
     [SerializeField] private bool canMoveHorizontally;
     [SerializeField] private bool canMoveVertically;
     [SerializeField] private bool canRotateHorizontally;
     [SerializeField] private bool canRotateVertically;
+    [Header("-----End Level Variable------")]
+    [SerializeField] private float lerpRotationSpeed = 1f;
+    [SerializeField] private float lerpPositionSpeed = 10f;
 
-    private Vector3 _initForward;
-
+    private Vector3 _initNormal;
+    private Vector3 _initPosition;
+    private Vector3 _fakeLightPoint;
+    private float _lightPointDistance;
     private void Start()
     {
-        _initForward = correctFormat.forward;
+        _initPosition = correctFormat.position;
+        _lightPointDistance = Vector3.Distance(correctFormat.position,lightPoint.position);
+        _initNormal = correctFormat.forward; //-(lightPoint.position - correctFormat.position).normalized;
+        
     }
 
-    public void Move(Vector3 movingDelta)
+    public void Move(Vector3 moveDirection,float movingSpeed)
     {
+        float moveDeltaSpeed = movingSpeed * Time.deltaTime;
+        Vector3 Horizontal = canMoveHorizontally ? Vector3.right : Vector3.zero;
+        Vector3 Vertical = canMoveVertically ? Vector3.up : Vector3.zero;
+        Vector3 movingDelta = Vertical * (moveDirection.y * moveDeltaSpeed) 
+                              + Horizontal * (moveDirection.x * moveDeltaSpeed);
         shadowOnly.Translate(movingDelta,Space.World);
         meshOnly.Translate(movingDelta,Space.World);
         correctFormat.Translate(movingDelta,Space.World);
+        correctFormat.forward = -(lightPoint.position - correctFormat.position).normalized;
     }
 
     public void Rotate(Vector3 rotationDir,float rotationSpeed)
     {
-        Vector3 rotHorizontal = canRotateHorizontally ? Vector3.up * -rotationDir.x * rotationSpeed : Vector3.zero;
-        Vector3 rotVertical = canRotateVertically ? Vector3.right * rotationDir.y * rotationSpeed : Vector3.zero;
-        Vector3 rotationDelta = rotHorizontal + rotVertical;
+        Vector3 rotHorizontal = canRotateHorizontally ? Vector3.up * (-rotationDir.x * rotationSpeed) : Vector3.zero;
+        Vector3 rotVertical = canRotateVertically ? Vector3.right * (rotationDir.y * rotationSpeed) : Vector3.zero;
+        Vector3 rotationDelta = (rotHorizontal + rotVertical) * Time.deltaTime;
         shadowOnly.Rotate(rotationDelta,Space.World);
         meshOnly.Rotate(rotationDelta,Space.World);
     }
@@ -50,16 +66,44 @@ public class ShadowObject : MonoBehaviour
         return isRotationCloseEnough;
     }
 
-    public IEnumerator LerpToRotation()
+    public bool CheckIfPositionCorrect()
+    {
+        float distance = Vector3.Distance(shadowOnly.position, correctFormat.position);
+        return distance < positionTolerance;
+    }
+    public IEnumerator LerpToCorrectRotation()
     {
         float angle = Quaternion.Angle(correctFormat.rotation, shadowOnly.rotation);
-        float deltaTimeSpeed = Time.deltaTime / angle;
+        float deltaTimeSpeed = Time.deltaTime / (angle * lerpRotationSpeed);
         float time = 0f;
-        while (time < 1f)
+        while (time <= 1f)
         {
             Quaternion q = Quaternion.Lerp(shadowOnly.rotation, correctFormat.rotation, time);
             meshOnly.rotation = q;
             shadowOnly.rotation = q;
+            time += deltaTimeSpeed;
+            yield return null;
+        }
+    }
+
+    public IEnumerator LerpToCorrectPosition()
+    {
+        float distance = Vector3.Distance(_initPosition, shadowOnly.position);
+        distance = distance > 0 ? distance * lerpPositionSpeed : lerpPositionSpeed;
+        float deltaTimeSpeed = Time.deltaTime / distance;
+        float time = 0f;
+        while (time <= 1f)
+        {
+            Vector3 newPos = Vector3.Lerp(shadowOnly.position, _initPosition, time);
+            Vector3 newForward = Vector3.Lerp(meshOnly.forward, _initNormal, time);
+            meshOnly.forward = newForward;
+            shadowOnly.forward = newForward;
+            correctFormat.forward = newForward;
+
+            correctFormat.position = newPos;
+            shadowOnly.position = newPos;
+            newPos.z = meshOnly.position.z;
+            meshOnly.position = newPos;
             time += deltaTimeSpeed;
             yield return null;
         }
